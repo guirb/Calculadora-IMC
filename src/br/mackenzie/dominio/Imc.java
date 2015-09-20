2f0927979349faf39d3e86793e24e1f0069897d9
guirb <guilherme.rib@terra.com.br>
2015-09-20 16:59:32 -0300

    Adicionado os teste

--- src/br/mackenzie/dominio/Imc.java
+++ src/br/mackenzie/dominio/Imc.java
@@ -13,10 +13,14 @@ public class Imc {
 
     private double imc;
     private String descricao;
+    private Pessoa pessoa;
+    private String mensagem;
 
-    public Imc(double imc, String descricao) {
+    public Imc(double imc, String descricao, Pessoa pessoa, String mensagem) {
         this.imc = imc;
         this.descricao = descricao;
+        this.pessoa = pessoa;
+        this.mensagem = mensagem;
     }
 
     public Imc() {
@@ -38,19 +42,41 @@ public class Imc {
         this.descricao = descricao;
     }
 
-    public void calcularImc(double peso, double altura) {
-        this.imc = peso / (Math.pow(altura, 2));
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public void calcularImc() {
+        this.imc = pessoa.getPeso() / (Math.pow(pessoa.getAltura(), 2));
+        validarImc();
     }
 
     public void validarImc() {
-        if (imc > 18.5) {
+        if (imc < 18.5) {
             this.descricao = "Abaixo do Peso";
-        } else if (imc > 25.0) {
+            this.mensagem = "Você está abaixo do peso recomendado.";
+        } else if (imc < 25.0) {
             this.descricao = "Peso Normal";
-        } else if (imc > 30.0) {
+            this.mensagem = "Você está muito bem! Continue assim!";
+        } else if (imc < 30.0) {
             this.descricao = "Acima do Peso";
+            this.mensagem = "Você está acima do peso recomendado. Cuidado!";
         } else {
             this.descricao = "Obeso";
+            this.mensagem = "Você está Obeso. Procure o acompanhamento de um "
+                    + "nutricionista e realizar mais atividades físicas!";
         }
     }
 
